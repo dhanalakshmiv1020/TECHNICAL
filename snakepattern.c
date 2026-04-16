7499429906f040befa9604817c58ea333e13fce0
dhanalakshmi v <dhanalakshmiv200628@gmail.com>
2026-04-16 09:42:03 +0530

    c project

---
+++ snakepattern.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n = 10;
+    int i,j;
+    char board[10][10];
+
+    char *ptr = &board[0][0];
+
+    // Fill board
+    for(i = 0; i < n*n; i++) {
+        *(ptr + i) = '.';
+    }
+
+    // Snake head
+    *(ptr + 22) = 'O';
+
+    // Print board
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            printf("%c ", board[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
